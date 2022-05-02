ae729dbe38263c53658cad51b5f49ffe1f4f09a8
Julia <julaschmid@googlemail.com>
2022-05-02 09:05:51 +0200

    some changes

--- PyPhyUtils/iqtree_topology_tests.py
+++ PyPhyUtils/iqtree_topology_tests.py
@@ -8,68 +8,6 @@ from .utils import run_cmd, read_file_contents, write_trees_to_file
 from tempfile import TemporaryDirectory
 
 
-def get_rfdist_clusters(log_path, all_trees):
-    content = read_file_contents(log_path)
-    clusters = []
-
-    for line in content:
-        line = line.strip()
-        if not line.startswith("["):
-            continue
-        # the line is a string representation of a list of ints
-        # like this: [1, 2, 3, 4, ]
-        tree_ids = eval(line)
-        cluster = set()
-
-        for id in tree_ids:
-            cluster.add(all_trees[id])
-
-        clusters.append(cluster)
-
-    if len(clusters) == 0:
-        raise RuntimeError(
-            "The given RAxML-NG logfile does not contain the clusters, make sure to use the adapted version of RAxML-NG that is available here: https://github.com/tschuelia/raxml-ng"
-        )
-
-    return clusters
-
-
-def filter_tree_topologies(
-    unfiltered_trees_file: FilePath,
-    raxmlng: RAxMLNG,
-) -> Tuple[List[NewickString], List]:
-    tree_strings = read_file_contents(unfiltered_trees_file)
-    num_trees = len(tree_strings)
-
-    if num_trees > 1:
-        with TemporaryDirectory() as tmpdir:
-            raxmlng_prefix = tmpdir + "raxmlng_rfdist"
-            raxmlng_rfdist_cmd = raxmlng.get_rfdist_cmd(
-                trees_file=unfiltered_trees_file, prefix=raxmlng_prefix
-            )
-            run_cmd(raxmlng_rfdist_cmd)
-            raxmlng_log_file = raxmlng_prefix + ".raxml.log"
-            num_topos = get_raxmlng_num_unique_topos(raxmlng_log_file)
-
-            if num_topos > 1:
-                clusters = get_rfdist_clusters(raxmlng_log_file, tree_strings)
-
-                assert len(clusters) == num_topos
-            else:
-                clusters = [set(tree_strings)]
-
-    else:
-        clusters = [set(tree_strings)]
-
-    # for each cluster: keep only one tree as representative of the cluster
-    filtered_trees = [next(iter(cluster)) for cluster in clusters]
-
-    # sanity checks
-    assert sum([len(s) for s in clusters]) <= num_trees
-
-    return filtered_trees, clusters
-
-
 def get_iqtree_results_for_tree(iqtree_results: TreeIndexed[IqTreeMetrics], tree: NewickString, clusters: List):
     # returns the results for this eval_tree_id as well as the cluster ID
     for i, cluster in enumerate(clusters):
@@ -102,7 +40,7 @@ class IQTreeTopologyTest:
             filtered_trees_file = tmpdir + "filtered.trees"
             write_trees_to_file(file_path=filtered_trees_file, trees=filtered_trees)
 
-            topology_test_cmd = self.iqtree.get_iqtree_tree_topology_test_cmd(
+            topology_test_cmd = self.iqtree.get_tree_topology_test_cmd(
                 msa_file=msa_file,
                 model=model,
                 treesfile=filtered_trees_file,
--- PyPhyUtils/raxml8.py
+++ PyPhyUtils/raxml8.py
@@ -1,71 +1,71 @@
 from .custom_types import *
 
 
-def _get_raxml_base_command(
-    raxml_executable: Executable,
-    msa: FilePath,
-    model: str,
-    suffix: str,
-    threads: int = 2,
-    **kwargs,
-) -> str:
-    additional_settings = []
-    for key, value in kwargs.items():
-        if value is None:
-            additional_settings += [f"-{key}"]
+class RAxML:
+    def __init__(self, exe_path: Executable):
+        self.exe_path = exe_path
+
+    def _base_cmd(
+        self,
+        msa: FilePath,
+        model: str,
+        suffix: str,
+        **kwargs,
+    ) -> Command:
+        additional_settings = []
+        for key, value in kwargs.items():
+            if value is None:
+                additional_settings += [f"-{key}"]
+            else:
+                additional_settings += [f"-{key}", str(value)]
+
+        return [
+            self.exe_path,
+            "-s",
+            msa,
+            "-m",
+            model,
+            "-n",
+            suffix,
+            *additional_settings,
+        ]
+
+    def get_tree_topology_test_command(
+        self,
+        msa: FilePath,
+        treesfile: FilePath,
+        model: str,
+        suffix: str,
+        test_algorithm: str,
+        best_tree_file: FilePath = None,
+        n_bootstrap_replicates: int = 10_000,
+        bootstrap_seed: int = 1,
+        **kwargs,
+    ) -> Command:
+
+        algo = ["-f"]
+        if test_algorithm == "ELW":
+            algo += ["W"]
+        elif test_algorithm == "SH":
+            algo += ["H"]
+        elif test_algorithm == "sitelh":
+            algo += ["G"]
         else:
-            additional_settings += [f"-{key}", str(value)]
+            raise ValueError(
+                f"Error: unrecognized topology test algorithm {test_algorithm}. Available options are `ELW` and `SH`."
+            )
 
-    return [
-        raxml_executable,
-        "-s",
-        msa,
-        "-m",
-        model,
-        "-n",
-        suffix,
-        "-T",
-        str(threads),
-        *additional_settings,
-    ]
+        best_tree = ["-t", best_tree_file] if best_tree_file else []
 
+        base_command = self._base_cmd(msa, model, suffix, **kwargs)
 
-def get_raxml_tree_topology_test_command(
-    raxml_executable: Executable,
-    msa: FilePath,
-    treesfile: FilePath,
-    model: str,
-    suffix: str,
-    test_algorithm: str,
-    best_tree_file: FilePath = None,
-    threads: int = 2,
-    n_bootstrap_replicates: int = 10_000,
-    **kwargs,
-) -> str:
-
-    algo = ["-f"]
-    if test_algorithm == "ELW":
-        algo += ["W"]
-    elif test_algorithm == "SH":
-        algo += ["H"]
-    elif test_algorithm == "sitelh":
-        algo += ["G"]
-    else:
-        raise ValueError(
-            f"Error: unrecognized topology test algorithm {test_algorithm}. Available options are `ELW` and `SH`."
-        )
-
-    best_tree = ["-t", best_tree_file] if best_tree_file else []
-
-    base_command = _get_raxml_base_command(
-        raxml_executable, msa, model, suffix, threads, **kwargs
-    )
-
-    return base_command + [
-        *algo,
-        "-z",
-        treesfile,
-        *best_tree,
-        "-N",
-        str(n_bootstrap_replicates),
-    ]
+        return base_command + [
+            *algo,
+            "-z",
+            treesfile,
+            *best_tree,
+            "-N",
+            str(n_bootstrap_replicates),
+            "-b",
+            str(bootstrap_seed),
+        ]
